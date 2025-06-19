c66e7be53e9e8e00ae0b85ac54e63ac7851e7c77
India Grant <india@alchemtechnologies.com>
2025-06-20 00:46:04 +0100

    style: make layout responsive

--- src/app/shared/pill/pill.ts
+++ src/app/shared/pill/pill.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, output, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-pill',
